1c30d9fd1c6b4f3628efd9acf0f453fb07e93eb6
robmllze <86869208+robmllze@users.noreply.github.com>
2025-01-28 14:18:40 +1100

    +chore: Added a Throttle class

---
+++ lib/src/_index.g.dart
@@ -0,0 +1,11 @@
+//.title
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// GENERATED - DO NOT MODIFY BY HAND
+// See: https://github.com/dev-cetera/df_generate_dart_indexes
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//.title~
+
+export './debouncer.dart';
+export './throttle.dart';
---
+++ lib/src/throttle.dart
@@ -0,0 +1,34 @@
+//.title
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// Dart/Flutter (DF) Packages by dev-cetera.com & contributors. The use of this
+// source code is governed by an MIT-style license described in the LICENSE
+// file located in this project's root directory.
+//
+// See: https://opensource.org/license/mit
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//.title~
+
+class Throttle {
+  final Duration duration;
+  bool _isThrottled = false;
+
+  Throttle(this.duration);
+
+  Throttle.zero() : duration = const Duration();
+  Throttle.to24hz() : duration = const Duration(milliseconds: 1000 ~/ 24);
+  Throttle.to30hz() : duration = const Duration(milliseconds: 1000 ~/ 30);
+  Throttle.to48hz() : duration = const Duration(milliseconds: 1000 ~/ 48);
+  Throttle.to60hz() : duration = const Duration(milliseconds: 1000 ~/ 60);
+  Throttle.to120hz() : duration = const Duration(milliseconds: 1000 ~/ 120);
+
+  bool get isThrottled => _isThrottled;
+
+  void run(void Function() action) {
+    if (_isThrottled) return;
+    _isThrottled = true;
+    action();
+    Future.delayed(duration, () => _isThrottled = false);
+  }
+}
